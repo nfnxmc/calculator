205ea28a28dd5ceb5baf1d552f4efc17d346ff26
nfnxmc <mamoudou.n.camara@gmail.com>
2022-07-18 22:36:19 +0200

    Step 4

--- src/main/java/org/mnc/Calculator.java
+++ src/main/java/org/mnc/Calculator.java
@@ -2,11 +2,35 @@ package org.mnc;
 
 
 import java.util.Arrays;
+import java.util.List;
+import java.util.Objects;
 
 public class Calculator {
     private static final String NEW_LINE = "\n";
+    public static final String DEFAULT_DELIMITER = ";";
+    public static final List<String> ESCAPES = List.of("|", "*", "?", "+", ".");
+
+    private String delimiter(String numbers) {
+        if (Objects.isNull(numbers) || numbers.isBlank() || !numbers.startsWith("//")) {
+            return DEFAULT_DELIMITER;
+        }
+        int firstIndexOfNewLine = numbers.indexOf("\n");
+        String separator = numbers.substring(2, firstIndexOfNewLine);
+        return ESCAPES.contains(separator) ? "\\" + separator : separator;
+    }
+
+    private int numbersStart(String numbers) {
+        if (Objects.isNull(numbers) || numbers.isBlank() || !numbers.startsWith("//")) {
+            return 0;
+        }
+
+        return numbers.indexOf("\n") + 1;
+    }
+
     public int add(String numbers) {
-        String[] operands = numbers.split(",");
+        String delimiter = delimiter(numbers);
+        int numbersStart = numbersStart(numbers);
+        String[] operands = numbers.substring(numbersStart).split(delimiter);
         if (operands.length > 1 && NEW_LINE.equals(operands[operands.length - 1])) throw new IllegalArgumentException();
         return Arrays.stream(operands).flatMap(s -> Arrays.stream(s.split("\\n"))).mapToInt(s -> s.isBlank() ? 0 : Integer.parseInt(s)).sum();
     }
--- src/test/java/org/mnc/CalculatorTest.java
+++ src/test/java/org/mnc/CalculatorTest.java
@@ -7,11 +7,12 @@ import static org.junit.Assert.assertEquals;
 public class CalculatorTest {
 
     @Test(expected = NumberFormatException.class)
-    public void step1NumberFormat(){
+    public void step1NumberFormat() {
         new Calculator().add("1,&");
     }
+
     @Test(expected = IllegalArgumentException.class)
-    public void step3EmptyLastLine(){
+    public void step3EmptyLastLine() {
         new Calculator().add("1,\n");
     }
 
@@ -23,12 +24,22 @@ public class CalculatorTest {
         assertEquals(1, calculator.add("1"));
         assertEquals(3, calculator.add("1,2"));
         assertEquals(45, calculator.add("0,1,2,3,4,5,6,7,8,9"));
-    } @Test
+    }
+
+    @Test
     public void step3() {
         Calculator calculator = new Calculator();
 
         assertEquals(0, calculator.add(""));
         assertEquals(1, calculator.add("1\n"));
         assertEquals(6, calculator.add("1\n2,3"));
+    }
+
+    @Test
+    public void step4() {
+        Calculator calculator = new Calculator();
+        assertEquals(0, calculator.add("//;\n"));
+        assertEquals(6, calculator.add("//|\n1\n2|3"));
+        assertEquals(6, calculator.add("//+\n1+2+3"));
     }
 }
